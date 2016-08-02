e777e2cd2d7174de9b0fef8b27f01a689d8721f0
Adriano Di Giovanni <me@adrianodigiovanni.com>
2016-08-02 21:09:29 +0200

    initial commit

---
+++ test/index.js
@@ -0,0 +1,142 @@
+var and = okay.and;
+var array = okay.array;
+var boolean = okay.boolean;
+var callIf = okay.callIf;
+var callIfNot = okay.callIfNot;
+var createRule = okay.createRule;
+var date = okay.date;
+var gt = okay.gt;
+var gte = okay.gte;
+var lt = okay.lt;
+var lte = okay.lte;
+var number = okay.number;
+var object = okay.object;
+var or = okay.or;
+var required = okay.required;
+var string = okay.string;
+
+describe('unit testing', function () {
+  it('createRule', function () {
+    expect(createRule).to.be.a.function;
+    var param = 0;
+    var resolve = function (value, param) {
+      return value > param;
+    };
+    var _resolve = sinon.spy(resolve);
+    var validate = createRule({
+      param: param,
+      resolve: _resolve
+    });
+    var value = 1;
+    expect(validate).to.be.a.function;
+    expect(validate.__param).to.equal(param);
+    expect(validate(value)).to.be.true;
+    expect(_resolve.calledOnce).to.be.true;
+    expect(_resolve.calledWith(value, param)).to.be.true;
+    param = function () {
+      return 0;
+    };
+    var _param = sinon.spy(param);
+    _resolve = sinon.spy(resolve);
+    validate = createRule({
+      param: _param,
+      resolve: _resolve
+    });
+    expect(validate(value)).to.be.true;
+    expect(_param.calledOnce).to.be.true;
+    expect(_resolve.calledOnce).to.be.true;
+    expect(_resolve.calledWith(value, param())).to.be.true;
+  });
+  it('and', function () {
+    var rule1 = sinon.stub().returns(true);
+    var rule2 = sinon.stub().returns(false);
+
+    var validate = and(rule1, rule2);
+    expect(validate).to.be.a.function;
+    expect(validate(1)).to.be.false;
+    expect(rule1.calledOnce).to.be.true;
+    expect(rule1.calledWith(1)).to.be.true;
+    expect(rule2.calledOnce).to.be.true;
+    expect(rule2.calledWith(1)).to.be.true;
+  });
+  it('array', function () {
+    expect(array()()).to.be.false;
+    expect(array()([])).to.be.true;
+  });
+  it('boolean', function () {
+    expect(boolean()()).to.be.false;
+    expect(boolean()(false)).to.be.true;
+    expect(boolean()(true)).to.be.true;
+  });
+  it('callIf', function () {
+    var rule = sinon.stub().returns(true);
+    var callback = sinon.spy();
+    var context = {};
+    expect(callIf(rule, callback, context)(1)).to.be.true;
+    expect(rule.calledOnce).to.be.true;
+    expect(rule.calledWith(1)).to.be.true;
+    expect(callback.calledOnce).to.be.true;
+    expect(callback.calledWith(1)).to.be.true;
+    expect(callback.calledOn(context)).to.be.true;
+  });
+  it('callIfNot', function () {
+    var rule = sinon.stub().returns(false);
+    var callback = sinon.spy();
+    var context = {};
+    expect(callIfNot(rule, callback, context)(1)).to.be.false;
+    expect(rule.calledOnce).to.be.true;
+    expect(rule.calledWith(1)).to.be.true;
+    expect(callback.calledOnce).to.be.true;
+    expect(callback.calledWith(1)).to.be.true;
+    expect(callback.calledOn(context)).to.be.true;
+  });
+  it('date', function () {
+    expect(date()()).to.be.false;
+    expect(date()(new Date())).to.be.true;
+  })
+  it('gt', function () {
+    expect(gt(0)(1)).to.be.true;
+    expect(gt(0)(0)).to.be.false;
+  });
+  it('gte', function () {
+    expect(gte(0)(0)).to.be.true;
+    expect(gte(0)(-1)).to.be.false;
+  });
+  it('lt', function () {
+    expect(lt(0)(-1)).to.be.true;
+    expect(lt(0)(0)).to.be.false;
+  });
+  it('lte', function () {
+    expect(lte(0)(0)).to.be.true;
+    expect(lte(0)(1)).to.be.false;
+  });
+  it('number', function () {
+    expect(number()()).to.be.false;
+    expect(number()(0)).to.be.true;
+  });
+  it('object', function () {
+    expect(object()()).to.be.false;
+    expect(object()({})).to.be.true;
+  });
+  it('or', function () {
+    var rule1 = sinon.stub().returns(false);
+    var rule2 = sinon.stub().returns(true);
+
+    var validate = or(rule1, rule2);
+    expect(validate).to.be.a.function;
+    expect(validate(1)).to.be.true;
+    expect(rule1.calledOnce).to.be.true;
+    expect(rule1.calledWith(1)).to.be.true;
+    expect(rule2.calledOnce).to.be.true;
+    expect(rule2.calledWith(1)).to.be.true;
+  });
+  it('required', function () {
+    expect(required()()).to.be.false;
+    expect(required()('')).to.be.false;
+    expect(required()('value')).to.be.true;
+  });
+  it('string', function () {
+    expect(string()()).to.be.false;
+    expect(string()('')).to.be.true;
+  });
+});
---
+++ test/okay.js
@@ -0,0 +1,198 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['lodash'], function (_) {
+      return (root.okay = factory(_));
+    });
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory(require('underscore'));
+  } else {
+    root.okay = factory(root._);
+  }
+}(this, function (_) {
+  var exports = {};
+
+  var isArray = _.isArray;
+  var isBoolean = _.isBoolean;
+  var isDate = _.isDate;
+  var isEmpty = _.isEmpty;
+  var isFunction = _.isFunction;
+  var isNumber = _.isNumber;
+  var isObject = _.isObject;
+  var isString = _.isString;
+  var map = _.map;
+  var reduce = _.reduce;
+  var toArray = _.toArray;
+
+  var createRule = exports.createRule = function createRule(descriptor) {
+    var param = descriptor.param;
+    var resolve = descriptor.resolve;
+    var rule = function (value) {
+      var _value = isFunction(value) ? value() : value;
+      var _param = isFunction(param) ? param() : param;
+      return resolve(_value, _param);
+    };
+    rule.__param = param;
+    return rule;
+  };
+
+  var compose = exports.compose = function compose() {
+    var rules = toArray(arguments);
+    return function (value) {
+      return map(rules, function (rule) {
+        return rule(value);
+      });
+    };
+  };
+
+  var iteratee$and = function iteratee$and(previousValue, currentValue) {
+    return previousValue && currentValue;
+  };
+  var resolve$and = function resolve$and(value, param) {
+    return reduce(compose.apply(null, param)(value), iteratee$and, true);
+  };
+  exports.and = function and() {
+    return createRule({
+      param: arguments,
+      resolve: resolve$and
+    });
+  };
+
+
+  var iteratee$or = function iteratee$or(previousValue, currentValue) {
+    return previousValue || currentValue;
+  };
+  var resolve$or = function resolve$or(value, param) {
+    return reduce(compose.apply(null, param)(value), iteratee$or, false);
+  };
+  exports.or = function or() {
+    return createRule({
+      param: arguments,
+      resolve: resolve$or
+    });
+  };
+
+  var resolve$callIf = function resolve$callIf(value, param) {
+    var rule = param.rule;
+    var callback = param.callback;
+    var context = param.context;
+    var returnValue = rule(value);
+    returnValue && callback.call(context, value, rule.__param);
+    return returnValue;
+  };
+  exports.callIf = function callIf(rule, callback, context) {
+    return createRule({
+      param: {
+        rule: rule,
+        callback: callback,
+        context: context
+      },
+      resolve: resolve$callIf
+    })
+  };
+
+  var resolve$callIfNot = function resolve$callIfNot(value, param) {
+    var rule = param.rule;
+    var callback = param.callback;
+    var context = param.context;
+    var returnValue = rule(value);
+    ! returnValue && callback.call(context, value, rule.__param);
+    return returnValue;
+  };
+  exports.callIfNot = function callIfNot(rule, callback, context) {
+    return createRule({
+      param: {
+        rule: rule,
+        callback: callback,
+        context: context
+      },
+      resolve: resolve$callIfNot
+    })
+  };
+
+  exports.array = function array() {
+    return createRule({
+      resolve: isArray
+    });
+  };
+
+  exports.boolean = function boolean() {
+    return createRule({
+      resolve: isBoolean
+    })
+  };
+
+  exports.date = function date() {
+    return createRule({
+      resolve: isDate
+    });
+  };
+
+  var resolve$gt = function resolve$gt(value, param) {
+    return value > param;
+  };
+  exports.gt = function gt(param) {
+    return createRule({
+      param: param,
+      resolve: resolve$gt
+    });
+  };
+
+  var resolve$gte = function resolve$gte(value, param) {
+    return value >= param;
+  };
+  exports.gte = function gte(param) {
+    return createRule({
+      param: param,
+      resolve: resolve$gte
+    });
+  };
+
+  var resolve$lt = function resolve$lt(value, param) {
+    return value < param;
+  };
+  exports.lt = function lt(param) {
+    return createRule({
+      param: param,
+      resolve: resolve$lt
+    });
+  };
+
+  var resolve$lte = function resolve$lte(value, param) {
+    return value <= param;
+  };
+  exports.lte = function lte(param) {
+    return createRule({
+      param: param,
+      resolve: resolve$lte
+    });
+  };
+
+  exports.number = function number() {
+    return createRule({
+      resolve: isNumber
+    });
+  };
+
+  exports.object = function object() {
+    return createRule({
+      resolve: isObject
+    });
+  };
+
+  var resolve$required = function resolve$required(value) {
+    return ! isEmpty(value);
+  };
+  exports.required = function required() {
+    return createRule({
+      resolve: resolve$required
+    });
+  };
+
+  exports.string = function string() {
+    return createRule({
+      resolve: isString
+    });
+  };
+
+  return exports;
+}));
